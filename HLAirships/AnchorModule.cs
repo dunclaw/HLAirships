de3d98b81dae2dd263831baae559878b9887ce7c
dunclaw <dunclaw@hotmail.com>
2016-10-12 19:14:07 -0700

    Make Anchor more stable for long-term parking

--- HLAirships/AnchorModule.cs
+++ HLAirships/AnchorModule.cs
@@ -6,147 +6,163 @@ namespace HLAirships
 {
 	class AnchorModule : PartModule
 	{
-		[KSPField(isPersistant = true, guiActive = true, guiName = "Anchor")]
-		Boolean anchored;
-		[KSPField(isPersistant = true, guiActive = true, guiName = "AutoAnchor")]
+		[KSPField(isPersistant = true, guiActive = true, guiName = "Anchored")]
+		Boolean Anchored;
+		[KSPField(isPersistant = true, guiActive = true, guiName = "Auto UnAnchor")]
 		Boolean autoAnchor;
+
 		[KSPField(isPersistant = true, guiActive = false)]
-		Vector3 anchorPosition = new Vector3(0f, 0f, 0f);
+		private Vector3 AnchorPosition = new Vector3(0f, 0f, 0f);
 		[KSPField(isPersistant = true, guiActive = false)]
-		Vector3 anchorVelocity = new Vector3(0f, 0f, 0f);
+		private Vector3 AnchorVelocity = new Vector3(0f, 0f, 0f);
+		[KSPField(isPersistant = true, guiActive = false)]
+		private Vector3 AnchorAcceleration = new Vector3(0f, 0f, 0f);
+		[KSPField(isPersistant = true, guiActive = false)]
+		private Vector3 AnchorAngularVelocity = new Vector3(0f, 0f, 0f);
+
 		[KSPField(isPersistant = true, guiActive = false)]
 		Vessel.Situations previousState = Vessel.Situations.LANDED;
 
-		private bool inactive = true;
+		//have you ever clicked "AirAnchored"? Rember to keep interesting things from happening
+		[KSPField(isPersistant = true, guiActive = false)]
+		public bool isActive = false;
 
 		private static Vector3 zeroVector = new Vector3(0f, 0f, 0f);
+
+		private bool inactivePart = true;
 
 		public override void OnStart(StartState state)
 		{
 			if (state != StartState.Editor)
 			{
-				InitControlWindowState();
+				InitBaseState();
 			}
 		}
 
-		private void InitControlWindowState()
+		private void InitBaseState()
 		{
 			if (vessel != null)
 			{
-				anchorPosition = vessel.GetWorldPos3D();
+				AnchorPosition = vessel.transform.position;
 				part.force_activate();
-				RememberPreviousState();
-				if (HLEnvelopeControlWindow.Instance != null && vessel.isActiveVessel)
-				{
-					HLEnvelopeControlWindow.Instance.AnchorPresent = true;
-					HLEnvelopeControlWindow.Instance.AnchorOn = anchored;
-					HLEnvelopeControlWindow.Instance.AutoAnchor = autoAnchor;
-					inactive = false;
-				}
-				else
-				{
-					inactive = true;
-				}
+			}
+		}
+
+		public override void OnSave(ConfigNode node)
+		{
+			base.OnSave(node);
+			if (vessel != null)
+			{
+				AnchorPosition = GetVesselPostion();
 			}
 		}
 
 		private void RememberPreviousState()
 		{
-			if (previousState != Vessel.Situations.LANDED)
+			if (!Anchored && previousState != Vessel.Situations.LANDED)
 			{
-				// don't overwrite the previous state
+				previousState = vessel.situation;
+				SetUpToolbarUI();
+			}
+		}
+
+		private void SetUpToolbarUI()
+		{
+			if (HLEnvelopeControlWindow.Instance != null && vessel.isActiveVessel)
+			{
+				HLEnvelopeControlWindow.Instance.AnchorPresent = true;
+				HLEnvelopeControlWindow.Instance.AnchorOn = Anchored;
+				HLEnvelopeControlWindow.Instance.AutoAnchor = autoAnchor;
+				inactivePart = false;
 			}
 			else
 			{
-				previousState = vessel.situation;
+				inactivePart = true;
 			}
 		}
 
 		public override void OnFixedUpdate()
 		{
-			// can't anchor if we're orbiting
+			// if we were initialized as inactive and are now active
+			if (inactivePart && vessel.isActiveVessel)
+			{
+				SetUpToolbarUI();
+			}
+			// if we were active, but are now inactive
+			if (!inactivePart && !vessel.isActiveVessel)
+			{
+				inactivePart = true;
+			}
+
+			// can't Anchor if we're orbiting
 			if (vessel.situation == Vessel.Situations.SUB_ORBITAL || vessel.situation == Vessel.Situations.ORBITING)
 			{
 				autoAnchor = false;
-				anchored = false;
+				Anchored = false;
 			}
-
-			// if we were initialized as inactive and are now active
-			if(inactive && vessel.isActiveVessel)
-			{
-				InitControlWindowState();
-			}
-			// if we were active, but are now inactive
-			if(!inactive && !vessel.isActiveVessel)
-			{
-				inactive = true;
-			}
-			// if we're inactive, see if we want to anchor 
+			// if we're inactive, and autoAnchor is set
 			if (!vessel.isActiveVessel && autoAnchor)
 			{
-				anchorPosition = vessel.GetWorldPos3D();
-				// if we're less than 1.5km from the active vessel and anchored, then wake up
-				if ((vessel.GetWorldPos3D() - FlightGlobals.ActiveVessel.GetWorldPos3D()).magnitude < 1500.0f && anchored)
+				// if we're less than 1.5km from the active vessel and Anchored, then wake up
+				if ((vessel.GetWorldPos3D() - FlightGlobals.ActiveVessel.GetWorldPos3D()).magnitude < 1500.0f && Anchored)
 				{
-					anchored = false;
 					vessel.GoOffRails();
-					vessel.Landed = false;
-					vessel.situation = previousState;
+					RestoreVesselState();
 				}
-				// if we're farther than 2km, auto anchor if needed
-				if ((vessel.GetWorldPos3D() - FlightGlobals.ActiveVessel.GetWorldPos3D()).magnitude > 2000.0f && (!anchored))
+				// if we're farther than 2km, auto Anchor if needed
+				if ((vessel.GetWorldPos3D() - FlightGlobals.ActiveVessel.GetWorldPos3D()).magnitude > 2000.0f && (!Anchored))
 				{
-					anchored = true;
 					AnchorVessel();
 				}
 			}
-			// if we're not anchored, and not active and flying, then go off rails
-			if ( !anchored && !vessel.isActiveVessel && vessel.situation == Vessel.Situations.FLYING)
+			if (Anchored)
 			{
-				vessel.GoOffRails();
+				AnchorVessel();
 			}
-			if (anchored)
+			if (HLEnvelopeControlWindow.Instance != null && vessel.isActiveVessel)
 			{
-				vessel.SetWorldVelocity(zeroVector);
-				vessel.acceleration = zeroVector;
-				vessel.angularVelocity = zeroVector;
-				vessel.geeForce = 0.0;
-				vessel.situation = Vessel.Situations.LANDED;
-				vessel.Landed = true;
-			}
-			if(HLEnvelopeControlWindow.Instance != null && vessel.isActiveVessel)
-			{
-				// save the previous state if needed
-				if(HLEnvelopeControlWindow.Instance.AnchorOn && !anchored)
-				{
-					RememberPreviousState();
-					AnchorVessel();
-				}
-				else if (!HLEnvelopeControlWindow.Instance.AnchorOn && anchored)
-				{
-					vessel.situation = previousState;
-				}
-				anchored = HLEnvelopeControlWindow.Instance.AnchorOn;
+				Anchored = HLEnvelopeControlWindow.Instance.AnchorOn;
 				autoAnchor = HLEnvelopeControlWindow.Instance.AutoAnchor;
 			}
+		}
+
+		private void RestoreVesselState()
+		{
+			if (isActive == false) { return; } //we only want to restore the state if you have Anchored somewhere intentionally
+			vessel.situation = previousState;
+			if (vessel.situation != Vessel.Situations.LANDED) { vessel.Landed = false; }
+			if (Anchored) { Anchored = false; }
+
+			FreezeVesselInPlace();
+
+			//Restore Velocity and Accleration
+			vessel.SetWorldVelocity(AnchorVelocity);
+			vessel.acceleration = AnchorAcceleration;
+			vessel.angularVelocity = AnchorAngularVelocity;
 		}
 
 		[KSPEvent(guiActive = true, guiName = "Toggle Anchor")]
 		public void ToggleAnchor()
 		{
-			// cannot anchor in orbit or sub-orbit
+			// cannot Anchor in orbit or sub-orbit
 			if (vessel.situation != Vessel.Situations.SUB_ORBITAL && vessel.situation != Vessel.Situations.ORBITING)
 			{
-				if (!anchored)
+				if (!Anchored)
 				{
+					AnchorPosition = GetVesselPostion();
+
+					//we only want to remember the initial velocity, not subseqent updates by onFixedUpdate()
+					AnchorVelocity = vessel.GetSrfVelocity();
+					AnchorAcceleration = vessel.acceleration;
+					AnchorAngularVelocity = vessel.angularVelocity;
+
 					AnchorVessel();
 				}
 				else
 				{
-					vessel.situation = previousState;
+					RestoreVesselState();
 				}
-				anchored = !anchored;
-				HLEnvelopeControlWindow.Instance.AnchorOn = anchored;
+				isActive = true;
 			}
 		}
 
@@ -155,14 +171,51 @@ namespace HLAirships
 		public void ToggleAutoAnchor()
 		{
 			autoAnchor = !autoAnchor;
-			HLEnvelopeControlWindow.Instance.AutoAnchor = autoAnchor;
 		}
 
 		private void AnchorVessel()
 		{
-			anchorPosition = vessel.GetWorldPos3D();
-			anchorVelocity = vessel.GetSrfVelocity();
 			RememberPreviousState();
+			FreezeVesselInPlace();
+
+			vessel.situation = Vessel.Situations.LANDED;
+			vessel.Landed = true;
+			Anchored = true;
 		}
+
+		private void FreezeVesselInPlace()
+		{
+			vessel.SetWorldVelocity(zeroVector);
+			vessel.acceleration = zeroVector;
+			vessel.angularVelocity = zeroVector;
+			vessel.geeForce = 0.0;
+			SetVesselPosition();
+
+		}
+
+		//Code Adapted from Hyperedit landing functions 
+		//https://github.com/Ezriilc/HyperEdit
+
+
+		private Vector3d GetVesselPostion()
+		{
+			double latitude = 0, longitude = 0, altitude = 0;
+			var pqs = vessel.mainBody.pqsController;
+			if (pqs == null)
+			{
+				Destroy(this);
+				return zeroVector;
+			}
+
+			altitude = pqs.GetSurfaceHeight(vessel.mainBody.GetRelSurfaceNVector(latitude, longitude)) - vessel.mainBody.Radius;
+
+			return vessel.mainBody.GetRelSurfacePosition(latitude, longitude, altitude);
+		}
+
+		private void SetVesselPosition()
+		{
+			vessel.orbitDriver.pos = AnchorPosition;
+		}
+
 	}
 }
